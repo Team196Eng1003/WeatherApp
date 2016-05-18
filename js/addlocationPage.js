cdfafcba57d6ba5ad6705b0f15d74d32d93cd7c8
RashedZaman <rzam4@student.monash.edu>
2016-05-18 15:43:34 +0800

    WeatherApp Skeleton Code

---
+++ js/addlocationPage.js
@@ -0,0 +1,9 @@
+// Code for the Add Location page.
+if (typeof(Storage) !== "undefined")
+{
+    console.log("localStorage is available.");
+}
+else
+{
+    console.log("localStorage is not supported by current browser.");
+}
